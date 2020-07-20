d65cf07922a544e352089e6095be51500ebe89c7
neetuchauhan0421 <neetuchauhan0421@gmail.com>
2020-07-20 23:02:00 +0530

    Updates

---
+++ precache-manifest.cca79998e87f921fa794e32eb622f186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fd0c118502628afdbf87a26f42b3c2",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "95b9b44bc328cc3b594a",
+    "url": "/mysite/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e10148b95f3ee5583497",
+    "url": "/mysite/static/js/2.f5d0d279.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/mysite/static/js/2.f5d0d279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b9b44bc328cc3b594a",
+    "url": "/mysite/static/js/main.420b6a41.chunk.js"
+  },
+  {
+    "revision": "1d99fe0da908ca569518",
+    "url": "/mysite/static/js/runtime-main.1d89857a.js"
+  }
+]);
